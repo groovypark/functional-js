54e1dad92761cbbde54d2d02bf7166215638c0d7
groovypark <soobin950@gmail.com>
2019-06-12 01:55:46 +0900

    데이터를 매끄럽게 체이닝하여 처리

--- Chapter1/1.3.2chain.js
+++ Chapter1/1.3.2chain.js
@@ -1,7 +1,26 @@
+let enrollment = [
+  {enrolled: 2, grade: 100},
+  {enrolled: 2, grade: 80},
+  {enrolled: 1, grade: 89}
+];
+
+// 명령형
+var totalGrades = 0;
+var totalStudentsFound = 0;
+for (let i = 0; i < enrollment.length; i++) {
+  let student = enrollment[i];
+  if (student !== null) {
+    if (student.enrolled > 1) {
+      totalGrades += student.grade;
+      totalStudentsFound++;
+    }
+  }
+}
+var avergae = totalGrades / totalStudentsFound; // 90
+
 // 체인
 _chain(enrollment)
   .filter(student => student.enrolled > 1)
   .pluck('grade')
   .avergae()
   .value(); // 90   _.value()를 호출해야 체인에 연결된 모든 연산들이 실행됩니다.
-  
